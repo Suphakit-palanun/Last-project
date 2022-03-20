d2f5fdcaf92750b7cf5677394d2b2b30b6d8b279
Supakit-palanun <you@example.com>
2022-03-20 22:54:58 +0700

    update

--- src/plugins/firebaseinit.js
+++ src/plugins/firebaseinit.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyA3Vh8MeNBItOOZZjapD2Xm9fvha4f0_m8",
-  authDomain: "data-firebase-a8a9a.firebaseapp.com",
-  projectId: "data-firebase-a8a9a",
-  storageBucket: "data-firebase-a8a9a.appspot.com",
-  messagingSenderId: "777048889675",
-  appId: "1:777048889675:web:68668535f29a5c671cf0ce",
-  measurementId: "G-NRFM1ZZT6C",
+    apiKey: "AIzaSyDfn0JZiGvgDc28hiixq-qhb28ZxysyzZo",
+    authDomain: "lastapp-47273.firebaseapp.com",
+    projectId: "lastapp-47273",
+    storageBucket: "lastapp-47273.appspot.com",
+    messagingSenderId: "424129372647",
+    appId: "1:424129372647:web:de7967e4874cdf32721d42",
+    measurementId: "G-424TW6Y4YF"
 });
 
 const auth = getAuth(firebaseApp);
